bd09a40378b99b6b09215fc9e04aecb6048f168c
Beirun <orillobernardjay@gmail.com>
2026-01-20 11:59:00 +0800

    added templates

---
+++ app/pages/agent/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    const route = useRoute()
+    const { id } = route.params
+</script>
+<template>
+    <div>
+        Agent {{ id }}
+    </div>
+</template>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,30 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['./app/assets/tailwind.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-  modules: ['@nuxt/image', '@nuxtjs/color-mode', 'v-gsap-nuxt']
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/color-mode',
+    'v-gsap-nuxt',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  }
 })
