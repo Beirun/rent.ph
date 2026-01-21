decbfceda27b389f4319ea48c930d75433ab5b4e
keulistin <christineannealesna@gmail.com>
2026-01-21 15:57:30 +0800

    Trial

---
+++ app/components/propertySearchBar.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+    <div>
+        <p>Hello World!</p>
+    </div>
+</template>
---
+++ app/pages/property.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import propertySearchBar from '~/components/propertySearchBar.vue'
+</script>
+
+<template>
+    <propertySearchBar />
+</template>
