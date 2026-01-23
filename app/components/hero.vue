74918b310907cfb5d956e686a751d8f4f550b847
Vulprit Prooze <raileyalin@gmail.com>
2026-01-23 11:32:26 +0800

    redesigned hero search

--- app/components/hero.vue
+++ app/components/hero.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { LiquidGlassCard } from '@/components/ui/liquid-glass'
+import { FancyButton } from './ui/fancy-button'
+
 const colorMode = useColorMode()
 const heroBg = computed(() => {
   if (colorMode.preference === 'light') {
@@ -18,46 +21,62 @@ const heroBg = computed(() => {
     <div class="text-black/60 dark:text-white/60 text-xl font-medium">
       Discover the perfect property in your ideal location
     </div>
-    <div class="flex bg-[#fafafa] dark:bg-[#212121] p-8 rounded-lg gap-4 border dark:border-gray-800 border-gray-200">
-      <div class="flex flex-col gap-4">
-        <Label for="location">Location</Label>
-        <Select id="location">
-          <SelectTrigger class="w-100 py-5">
-            <SelectValue>
-              <Icon name="lucide:map-pin" />
-              <Label>Select/Enter a City</Label>
-            </SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="1">1</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+    <!-- <div class="flex bg-[#fafafa] dark:bg-[#212121] p-8 rounded-lg gap-4 border dark:border-gray-800 border-gray-200"> -->
+    <LiquidGlassCard class="p-12 mt-12 border-0 bg-white/20 dark:bg-[#212121]/80" borderRadius="5rem"
+      glowIntensity="none" blurIntensity="sm" shadowIntensity="xs">
+      <div class="flex gap-4 items-center">
+        <div class="flex">
+          <div class="flex flex-col gap-4">
+            <!-- <Label for="location">Location</Label> -->
+            <Select id="location">
+              <SelectTrigger
+                class="w-100 py-5 color-base rounded-full rounded-tr-none rounded-br-none bg-neutral-100/70 dark:bg-neutral-900/70 dark:border-0">
+                <SelectValue>
+                  <Icon name="lucide:map-pin" />
+                  <Label>Select a City</Label>
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="1">1</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div class="flex flex-col gap-4">
+            <!-- <Label for="type">Type</Label> -->
+            <Select id="type">
+              <SelectTrigger
+                class="w-75 py-5 color-base rounded-full ml-1 rounded-tl-none bg-neutral-100/70 dark:bg-neutral-900/70 rounded-bl-none dark:border-gray-800/70 dark:border-0">
+                <SelectValue>
+                  <Icon name="lucide:home" />
+                  <Label>Select a property type</Label>
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="1">1</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div class="flex h-full items-end">
+          <FancyButton
+            class="gap-2 px-6 h-12 rounded-full border-0 from-[var(--primary-orange)] to-[var(--color-orange-700)] dark:from-[var(--primary-orange)] dark:to-[var(--color-orange-700)]"
+            glowColor="var(--color-orange-400)">
+            <div class="flex items-center">
+              <Icon name="lucide:search" class="bg-[#fafafa] dark:bg-[#121212]" />
+              <span class="pl-1 text-[#fafafa] dark:text-[#121212]">Search</span>
+            </div>
+          </FancyButton>
+        </div>
       </div>
-      <div class="flex flex-col gap-4">
-        <Label for="type">Type</Label>
-        <Select id="type">
-          <SelectTrigger class="w-75 py-5">
-            <SelectValue placeholder="Any" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="1">1</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div class="flex h-full items-end">
-        <Button class="gap-2 px-6 h-12">
-          <Icon name="lucide:search" class="bg-[#fafafa] dark:bg-[#121212]" />
-          Search
-        </Button>
-      </div>
-    </div>
+      <!-- </div> -->
+    </LiquidGlassCard>
   </div>
 </template>
