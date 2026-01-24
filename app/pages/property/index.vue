0f6ac733c2ebf4985e97bcc54c940946fca5c8a5
DarelBG <brylgorgonio@gmail.com>
2026-01-24 12:09:00 +0800

    Changes on propery listing page

--- app/pages/property/index.vue
+++ app/pages/property/index.vue
@@ -3,6 +3,7 @@ import propertySearchBar from '~/components/propertySearchBar.vue'
 import navbar from '~/components/navbar.vue'
 import pagination from '~/components/pagination.vue'
 import PropertiesPage from '~/components/propertiesPage.vue'
+import propertiesAndListings2 from '~/components/propertiesAndListings2.vue'
 
 const currentPage = ref(1)
 const totalPages = ref(10)
@@ -21,10 +22,10 @@ const handlePageChange = (page: number) => {
     <div class="container mx-auto px-10 pt-30 pb-20">
       <h1 class="text-4xl font-semibold mb-8">Property for Rent</h1>
       <ClientOnly>
-        <propertySearchBar />
+        <PropertiesAndListings2 />
       </ClientOnly>
 
-      <div class="mt-12 space-y-6">
+      <!-- <div class="mt-12 space-y-6">
         <PropertiesPage />
 
         <div class="flex justify-center">
@@ -34,7 +35,7 @@ const handlePageChange = (page: number) => {
             @page-change="handlePageChange"
           />
         </div>
-      </div>
+      </div> -->
     </div>
     <ClientOnly>
       <Footer />
