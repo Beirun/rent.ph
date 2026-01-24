3be9dd56780e0c47300ba7bb850fffb635529d93
Beirun <orillobernardjay@gmail.com>
2026-01-24 13:28:23 +0800

    Update hero component

--- app/components/hero.vue
+++ app/components/hero.vue
@@ -4,65 +4,84 @@ import { gsap } from 'gsap'
 const colorMode = useColorMode()
 const heroBg = computed(() => {
   if (colorMode.preference === 'light') {
-    return ` bg-[linear-gradient(to_top,rgba(255,255,255,1),transparent_25%),url('/hero.jpg')]`
+    return ` bg-[linear-gradient(to_top,rgba(255,255,255,1),transparent_15%),url('/hero.jpg')]`
   } else {
-    return `bg-[linear-gradient(to_top,rgba(10,10,10,1)_0%,transparent_25%),url('/hero-dark.jpg')]`
+    return `bg-[linear-gradient(to_top,rgba(18,18,18,1)_0%,transparent_15%),url('/hero-dark.jpg')]`
   }
 })
 
 const titleRef = ref<HTMLElement>()
 const subtitleRef = ref<HTMLElement>()
+const searchRef = ref<HTMLElement>()
 
 onMounted(() => {
   const titleElement = titleRef.value
   const subtitleElement = subtitleRef.value
-  if (!titleElement || !subtitleElement) return
+  const searchElement = searchRef.value
+  if (!titleElement || !subtitleElement || !searchElement) return
 
   // Entrance animation for "Rent Your Space"
-  gsap.fromTo(titleElement, 
+  gsap.fromTo(
+    titleElement,
     {
       opacity: 0,
-      y: 50,
-      scale: 0.8
+      y: 15,
     },
     {
       opacity: 1,
       y: 0,
-      scale: 1,
       duration: 1.5,
       ease: 'power3.out',
-      delay: 0.5
-    }
+      delay: 0.25,
+    },
   )
-
   // Entrance animation for subtitle
-  gsap.fromTo(subtitleElement, 
+  gsap.fromTo(
+    subtitleElement,
     {
       opacity: 0,
-      y: 30
+      y: 15,
     },
     {
       opacity: 1,
       y: 0,
       duration: 1.2,
       ease: 'power2.out',
-      delay: 1.0
-    }
+      delay: 0.5,
+    },
+  )
+  gsap.fromTo(
+    searchElement,
+    {
+      opacity: 0,
+      y: 15,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: 'power2.out',
+      delay: 0.75,
+    },
   )
 })
 </script>
 <template>
   <!-- Adjust padding bottom or top to move hero center elements -->
-  <div :class="[
-    'h-[115vh] bg-top bg-cover w-screen flex flex-col items-center justify-center gap-5 pb-52',
-    heroBg,
-  ]">
-    <div ref="titleRef" class="text-[#FE8E0A] text-8xl font-black">Rent Your Space</div>
+  <div
+    :class="[
+      'h-[75vh] bg-position-[100%_25%] bg-cover w-screen flex flex-col items-center justify-center gap-5',
+      heroBg,
+    ]"
+  >
+    <div ref="titleRef" class="text-[#FE8E0A] text-8xl font-extrabold">Rent Your Space</div>
     <div ref="subtitleRef" class="text-black/60 dark:text-white/60 text-xl font-medium">
       Discover the perfect property in your ideal location
     </div>
     <div
-      class="flex bg-[#fafafa] dark:bg-[#212121] mt-12 p-8 rounded-lg gap-4 border dark:border-gray-800 border-gray-200">
+      ref="searchRef"
+      class="flex bg-[#fafafa] dark:bg-[#212121] mt-12 p-8 rounded-lg gap-4 border dark:border-gray-800 border-gray-200"
+    >
       <div class="flex flex-col gap-4">
         <Label for="location">Location</Label>
         <Select id="location">
