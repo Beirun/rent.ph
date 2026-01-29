ae315e58bcae2f6e2c3f06912e506188958bc368
Beirun <orillobernardjay@gmail.com>
2026-01-29 10:16:59 +0800

    Update manager model

--- app/models/manager.ts
+++ app/models/manager.ts
@@ -1,9 +1,27 @@
-export interface Pagination {
-  current_page: number
-  last_page: number
-  per_page: number
-  total: number
-  from: number
-  to: number
-  has_more: boolean
+import type { Pagination } from './pagination'
+
+export interface AgentListResponse {
+  status: 'success'
+  data: Agent[]
+  pagination: Pagination
+  filters: Filters
+  message: string
+}
+
+export interface Agent {
+  id: number
+  name: string
+  first_name: string
+  last_name: string
+  email: string
+  phone: string
+  city: string | null
+  state: string | null
+  rmpro: string | null
+  agent_title: string
+  listing_count: number
+}
+
+export interface Filters {
+  order: string | null
 }
