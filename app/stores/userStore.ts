bb752768eab6d772d2ed769be5b0c2d11066daf3
DarelBG <brylgorgonio@gmail.com>
2026-02-11 14:27:32 +0800

    Created userStore for user profile

---
+++ app/stores/userStore.ts
@@ -0,0 +1,63 @@
+import { defineStore } from 'pinia'
+import type { UserProfile } from '@/models/user'
+
+const BASE_URL = 'https://rent.ph/api'
+
+export const useUserStore = defineStore('user', () => {
+    const user = ref<UserProfile | null>(null)
+    const loading = ref(false)
+
+    const fullName = computed(() => {
+        if (!user.value) return ''
+        return `${user.value.first_name} ${user.value.last_name}`
+    })
+
+
+    const fetchUserProfile = async () => {
+        loading.value = true
+        const token = useCookie('access_token')
+
+        try {
+            const res: any = await $fetch(`${BASE_URL}/api/rent-manager-details/${user.value?.id}}`, {
+                headers: { Authorization: `Bearer ${token.value}` }
+            })
+            user.value = res.data
+        } catch (error) {
+            console.error('Failed to fetch profile:', error)
+        } finally {
+            loading.value = false
+        }
+    }
+
+    const updateUserProfile = async (formData: Partial<UserProfile>) => {
+        loading.value = true
+        const token = useCookie('access_token')
+
+        try {
+            const res: any = await $fetch(`${BASE_URL}'}/api/rent-manager-details/${user.value?.id}`, {
+                method: 'PUT',
+                headers: { Authorization: `Bearer ${token.value}` },
+                body: formData
+            })
+            
+            if (user.value) {
+                user.value = { ...user.value, ...formData }
+            }
+            
+            return { success: true }
+        } catch (error) {
+            console.error('Update failed:', error)
+            return { success: false, error }
+        } finally {
+            loading.value = false
+        }
+    }
+
+    return {
+        user,
+        fullName,
+        loading,
+        fetchUserProfile,
+        updateUserProfile
+    }
+})
